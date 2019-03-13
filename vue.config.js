1b9205d5d3e4a9855254d111d39c1dbccd45265d
Zach Toben <zach.toben@gmail.com>
2019-03-13 12:29:47 -0500

    add pwa manifest webpack plugin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,29 @@
+const path = require("path");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
 module.exports = {
   devServer: {
     port: 9999,
     open: true
   },
-  publicPath: "/"
+  publicPath: "/",
+  configureWebpack: {
+    plugins: [
+      new WebpackPwaManifest({
+        name: "Neon Maze",
+        short_name: "neon-maze",
+        description: "A colorful maze",
+        background_color: "lightgray",
+        orientation: "portrait",
+        display: "standalone",
+        start_url: ".",
+        icons: [
+          {
+            src: path.resolve("src/assets/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512]
+          }
+        ]
+      })
+    ]
+  }
 };
